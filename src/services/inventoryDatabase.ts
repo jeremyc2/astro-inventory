3ccedc7522b5a769fee82163febc44266744db1e
Jeremy Chandler <jeremychandler94@gmail.com>
2022-11-22 00:30:54 -0600

    create item function

--- src/services/inventoryDatabase.ts
+++ src/services/inventoryDatabase.ts
@@ -1,24 +1,23 @@
 import { z } from "zod";
+import { doc, setDoc } from "firebase/firestore"; 
+import { db } from "../firebase"
 
-const User = z.object({
+const Item = z.object({
     contributors: z.array(z.string()),
     name: z.string(),
     description: z.string().optional(),
     photoURLs: z.array(z.string().url()).optional()
 });
 
-type User = z.infer<typeof User>;
+type Item = z.infer<typeof Item>;
 
-export function createItem(newUser: User) {
-
-}
-
-export function getItem() {
-
+export async function createItem(id: string, newItem: Item) {
+    Item.parse(newItem);
+    return setDoc(doc(db, "items", id), newItem)
 }
 
 export function getItems() {
-
+    
 }
 
 export function updateItem() {
