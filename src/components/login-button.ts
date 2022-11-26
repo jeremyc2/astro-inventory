082760e33193741dfab95cf4ec316e7a1764a03b
Jeremy Chandler <jeremychandler94@gmail.com>
2022-11-25 18:53:27 -0600

    added firebase login

--- src/components/login-button.ts
+++ src/components/login-button.ts
@@ -1,12 +1,28 @@
 import { css, html, LitElement } from "lit";
 import { customElement } from "lit/decorators.js";
 import base from "../styles/lit-tw-base";
+import { signIn } from "../firebase/auth-popup";
+import type { User } from "firebase/auth";
+
+declare global {
+    interface Window { user: User; token: string }
+}
 
 @customElement("login-button")
 export class LoginButton extends LitElement {
     static styles = [base, css`@tailwind utilities`]
 
+    private async clickHandler() {
+        const loginInfo = await signIn();
+        if(!loginInfo) return
+
+        window.user = loginInfo.user;
+        window.token = loginInfo.token ?? "";
+    }
+
     render() {
-        return html`<button>Login</button>`
+        return html`<button class="h-10 px-6 font-semibold rounded-md bg-black text-white" @click="${this.clickHandler}">
+            Login
+        </button>`
     }
 }
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -19,3 +19,5 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
+
+console.log("firebase loaded")
